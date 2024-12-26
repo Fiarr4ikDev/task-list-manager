66be0adb942cb6d9046551abbf2ef5ab2bda99b7
fiarr4ik <fiarr4ik@gmail.com>
2024-12-26 12:38:01 +0700

    init commit

---
+++ src/main/java/ru/fiarr4ik/tasklistmanager/TaskListManagerApplication.java
@@ -0,0 +1,13 @@
+package ru.fiarr4ik.tasklistmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskListManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskListManagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/fiarr4ik/tasklistmanager/TaskListManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.fiarr4ik.tasklistmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskListManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
