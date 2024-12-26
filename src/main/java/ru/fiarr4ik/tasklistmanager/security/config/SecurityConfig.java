7db8d562e0510d1e8ae69ccb7be3c175d66b5f6d
fiarr4ik <fiarr4ik@gmail.com>
2024-12-26 14:27:28 +0700

    feat: сделал сервис для конфигурации безопасности: PasswordEncoder - BCryptPasswordEncoder UserDetailsService - кастомный UserDetailsServiceImpl Запихал всё это в провайдер Настроил простую цепочку фильтров с отключением csrf и требованием аутентифицировать любой запрос

---
+++ src/main/java/ru/fiarr4ik/tasklistmanager/security/config/SecurityConfig.java
@@ -0,0 +1,58 @@
+package ru.fiarr4ik.tasklistmanager.security.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import ru.fiarr4ik.tasklistmanager.security.UserDetailsServiceImpl;
+import ru.fiarr4ik.tasklistmanager.service.UserService;
+
+@Configuration
+@EnableMethodSecurity
+public class SecurityConfig {
+
+    private final UserService userService;
+
+    @Autowired
+    public SecurityConfig(UserService userService) {
+        this.userService = userService;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return new UserDetailsServiceImpl(userService);
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setPasswordEncoder(passwordEncoder());
+        provider.setUserDetailsService(userDetailsService());
+        return provider;
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable);
+
+        http.authorizeHttpRequests(request -> request
+                .anyRequest().authenticated()
+        );
+
+        return http.build();
+    }
+
+}
